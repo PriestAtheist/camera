49d8302573d708de9a98c13a89e400a23312e30c
wex333 <kobelyan05@mail.ru>
2023-10-11 18:25:26 +0300

    add

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path ="camera/room/:id" element={<Room/>}/>
-        <Route exact path ="camera/" element={<Main/>}/>
-        <Route path ="camera/*" element={<NotFound404/>}/>
+        <Route exact path ="/camera/room/:id" element={<Room/>}/>
+        <Route exact path ="/camera/" element={<Main/>}/>
+        <Route path ="/camera/*" element={<NotFound404/>}/>
       </Routes>
     </BrowserRouter>
   );
