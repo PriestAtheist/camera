8712fd98db89edc40e95b38cfed9e9fd75d3635c
wex333 <kobelyan05@mail.ru>
2023-10-11 18:36:57 +0300

    add

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
     transports: ["websocket"]
 }
 
-const socket = io('https://priestatheist.github.io/camera/', options);
+const socket = io('https://priestatheist.github.io/camera/:3001', options);
 
 export default socket;
