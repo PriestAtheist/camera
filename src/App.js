404a509cb1f924df5e50ae6e58fe01a9f32bc1e4
wex333 <kobelyan05@mail.ru>
2023-10-11 18:07:48 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Room from './pages/Room/index';
+import Main from './pages/Main/index';
+import NotFound404 from './pages/NotFound404/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path ="/room/:id" element={<Room/>}/>
+        <Route exact path ="/" element={<Main/>}/>
+        <Route path ="*" element={<NotFound404/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
